8a190169b14dcb23577ec757899c242948123e0f
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2024-01-09 12:23:01 +0300

    first commit

---
+++ src/test/java/pages/userHomePage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class userHomePage {
+}
+    git init
+    git add .
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/UgurcanAydogan/EasyBusTicket_UlFrameWork.git
+        git push -u origin main
